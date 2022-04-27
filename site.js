1ac02d0c3d6f92cb8c79020e7dcc8832000cac8e
kay4yk13 <denis.klimovich.web@gmail.com>
2022-04-27 22:32:28 +0300

    js catalog 2 method

--- site.js
+++ site.js
@@ -1,69 +1,99 @@
 // prettier-ignore
-let poops = [
+const poops = [
     {
-      "title": "Kal tvoey mamashi",
-      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      "img": "http://via.placeholder.com/150x150",
-      "quantity": 2000,
-      "price": 108,
+        title: 'Kal tvoey mamashi',
+        description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        img: 'http://via.placeholder.com/150x150',
+        quantity: 2000,
+        price: 108,
     },
     {
-        "title": "Drisnya 9000",
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "img": "http://via.placeholder.com/150x150",
-        "quantity": 1765,
-        "price": 100,
-      },
-      {
-        "title": "Podliva",
-        "description": "5656Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "img": "http://via.placeholder.com/150x150",
-        "quantity": 10,
-        "price": 223,
-      },
-      {
-        "title": "Drisnya",
-        "description": "12Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "img": "http://via.placeholder.com/150x150",
-        "quantity": 987,
-        "price": 2223,
-      },
-      {
-        "title": "Zhizha Zhizhi",
-        "description": "343Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "img": "http://via.placeholder.com/150x150",
-        "quantity": 1488,
-        "price": 666,
-      },
-      {
-        "title": "Govna pyatno",
-        "description": "444Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "img": "http://via.placeholder.com/150x150",
-        "quantity": 999,
-        "price": 999,
-      },
+        title: 'Drisnya 9000',
+        description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        img: 'http://via.placeholder.com/150x150',
+        quantity: 1765,
+        price: 100,
+    },
+    {
+        title: 'Podliva',
+        description:
+            "5656Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        img: 'http://via.placeholder.com/150x150',
+        quantity: 10,
+        price: 223,
+    },
+    {
+        title: 'Drisnya',
+        description:
+            "12Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        img: 'http://via.placeholder.com/150x150',
+        quantity: 987,
+        price: 2223,
+    },
+    {
+        title: 'Zhizha Zhizhi',
+        description:
+            "343Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        img: 'http://via.placeholder.com/150x150',
+        quantity: 1488,
+        price: 666,
+    },
+    {
+        title: 'Govna pyatno',
+        description:
+            "444Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        img: 'http://via.placeholder.com/150x150',
+        quantity: 999,
+        price: 999,
+    },
 ]
 
-function renderCatalogProducts() {
-    var productsHTML = getProductsHTML()
-    let parentNode = document.getElementById('product-grid')
+function productRenderMethod1() {
+    console.log('method1')
+    let productsHTML = ''
+
+    poops.forEach(function (product) {
+        let template = `<div class="product-item">
+          <div class="product-item-h1">${product.title}</div>
+          <img src="${product.img}" />
+          <div class="product-item-p">${product.description}</div>
+          <span class="quantity">${product.quantity} gr</span>
+          <span class="cost">${product.price} $</span>
+          <a href="/cart.html" class="tocartbutton">ADD to cart</a>
+          </div>`
+        productsHTML += template
+    })
+
+    const parentNode = document.getElementById('products-grid')
     parentNode.innerHTML = productsHTML
 }
 
-function getProductsHTML() {
-    let productsHTML = ''
-    poops.forEach(function (product) {
-        let template = `<div class="product-item">
-        <div class="product-item-h1">${product.title}</div>
-        <img src="${product.img}" />
-        <div class="product-item-p">${product.description}</div>
-        <span class="quantity">${product.quantity} gr</span>
-        <span class="cost">${product.price} $</span>
-        <a href="/cart.html" class="tocartbutton">ADD to cart</a>
-        </div>`
-        productsHTML += template
+function productRenderMethod2() {
+    console.log('method2')
+    const parentNode = document.getElementById('products-grid')
+
+    poops.forEach((product) => {
+        let productNode = document.createElement('div')
+        productNode.classList.add('product-item')
+        productNode.innerHTML = `
+            <div class="product-item-h1">${product.title}</div>
+            <img src="${product.img}" />
+            <div class="product-item-p">${product.description}</div>
+            <span class="quantity">${product.quantity} gr</span>
+            <span class="cost">${product.price} $</span>
+            <a href="/cart.html" class="tocartbutton">ADD to cart</a>`
+        parentNode.appendChild(productNode)
     })
-    return productsHTML
+}
+
+function renderCatalogProducts() {
+    if (Math.floor(Math.random() * (15 - 1)) % 2 != 0) {
+        productRenderMethod1()
+    } else {
+        productRenderMethod2()
+    }
 }
 
 renderCatalogProducts()
